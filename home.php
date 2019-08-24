e0605a4c887568e6dc7c1d611b9b4c884a3ef69b
yusrilfirza <yusrilfirza@github.com>
2019-08-24 20:39:32 +0700

    Initial commit

---
+++ home.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Miniweb</title>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/fonts/font-awesome.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Alex+Brush">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Arizonia">
+    <link rel="stylesheet" href="assets/css/styles.css">
+</head>
+
+<body style="background-color:rgb(245,245,245);">
+    <header class="header">
+        <nav class="navbar navbar-light navbar-expand-md">
+            <div class="container-fluid"><a class="navbar-brand" href="#" style="font-family:Arizonia, cursive;font-size:26px;">Beard</a><button class="navbar-toggler" data-toggle="collapse" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+                <div
+                    class="collapse navbar-collapse" id="navcol-1">
+                    <ul class="nav navbar-nav ml-auto">
+                        <li class="nav-item" role="presentation"><a class="nav-link active" href="#"><a class="btn btn-primary" role="button" href="index.html" id="out" style="background-color:rgb(63,130,98);"><i class="fa fa-sign-out"></i>&nbsp;Keluar</a></a>
+                        </li>
+                    </ul>
+            </div>
+            </div>
+        </nav>
+    </header>
+    <div>
+        <div class="container" style="margin-top:169px;">
+            <div class="col">
+                <div class="row" style="height:67px;">
+                    <div class="col">
+                        <h1 class="text-center" style="font-family:'Alex Brush', cursive;">Selamat Datang</h1>
+                        <p class="text-center" style="margin-top:-17px;"><i class="fa fa-scissors"></i></p>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-xl-4 align-self-center"><img src="assets/img/isometric.png" style="width:377px;"></div>
+                    <div class="col">
+                        <p>Barber berasal dari bahas Latin&nbsp;<em>"barba"</em>&nbsp;berarti&nbsp;<em>"Janggut"</em>. janggut selalu diidentikan dengan laki-laki/ pria.<br>Menurut kamus bahasa Inggris, barber adalah orang yang bekerja mencukur rambut.<br>Barbershop
+                            adalah tempat untuk mencukur rambut dan merapikan janggut pria.<br>Di Indonesia, sebagian orang lebih mengenal sebutan pangkas rambut dibandingkan dengan "Barbershop".<br>Barbershop adalah pangkas rambut, pangkas rambut adalah
+                            Barbershop.<br>Sebutan yang berbeda, membuat sebagian orang membedakan barbershop dan pangkas rambut dilihat dari kebersihan, suasana, dan lokasi.<br>Pangkas rambut identik dengan kesan kotor, panas, dan kumuh. Sedangkan barbershop
+                            adalah tempat yang bersih, tenang, sejuk ( memiliki pendingin udara/ AC ).<br><br></p>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col">
+                        <p class="text-center"><i class="fa fa-laptop"></i></p>
+                        <p class="text-center" style="margin-top:-19px;">Web masih dalam perbaikan</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <footer class="footer" style="background-color:#1c3227;padding:13px;">
+        <p class="text-center" style="color:rgb(242,242,242);margin-bottom:3px;">Copyright&nbsp;© BEARD Barbershop<br></p>
+    </footer>
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Miniweb</title>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/fonts/font-awesome.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Alex+Brush">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Arizonia">
+    <link rel="stylesheet" href="assets/css/styles.css">
+</head>
+
+<body style="background-color:rgb(245,245,245);">
+    <div>
+        <div class="container" style="margin-top:215px;width:644px;">
+            <div class="row">
+                <div class="col-lg-5 align-self-center">
+                    <p class="text-left"><img src="assets/img/Template.png" style="width:195px;"></p>
+                </div>
+                <div class="col">
+                    <form>
+                        <h1 style="font-family:'Alex Brush', cursive;">Masuk</h1>
+                        <div class="form-group"><input class="form-control" type="email" placeholder="Email"></div>
+                        <div class="form-group"><input class="form-control" type="password" placeholder="Password"></div>
+                        <div class="form-group"><a class="btn btn-primary" role="button" href="home.html" style="background-color:rgb(63,130,98);">Masuk</a>
+                            <div class="form-row" style="margin-top:9px;">
+                                <div class="col-lg-7">
+                                    <p>Lupa password? Klik&nbsp;<a href="#" style="color:rgb(137,164,138);">disini</a></p>
+                                </div>
+                                <div class="col">
+                                    <p class="text-right" style="margin-top:0px;">Daftar&nbsp;<a href="#" style="color:rgb(137,164,138);">disini</a></p>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+    <footer class="footer" style="background-color:#1c3227;padding:13px;">
+        <p class="text-center" style="color:rgb(242,242,242);margin-bottom:3px;">Copyright&nbsp;© BEARD Barbershop<br></p>
+    </footer>
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+</body>
+
+</html>
